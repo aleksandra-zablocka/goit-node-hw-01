13d138df8c4d68b488832b5790e78e583232c9a5
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-10-17 16:52:04 +0200

    finished all steps. added contacts.json and contacts.js with functions which I used in index.js

---
+++ contacts.js
@@ -0,0 +1,43 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log("Something went wrong", error);
+    throw error;
+  }
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((c) => c.id === contactId);
+  return contact;
+}
+
+async function addContact(name, email, phone) {
+  const newContact = { id: uuidv4(), name, email, phone };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const updatedContacts = contacts.filter((c) => c.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello world");
+const { listContacts, getContactById, removeContact, addContact } = require("./contacts");
+const { Command } = require("commander");
+
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.log("Lista kontaktów:", contacts);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      if (contact) {
+        console.log("Your contact:");
+        console.table([contact]);
+      } else {
+        ("Contact not found");
+      }
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log("New contact added:");
+      console.table([newContact]);
+      break;
+
+    case "remove":
+      await removeContact(id);
+      console.log("Contact removed.");
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
